c2e98c3a66b8323a4039a16b795f3438efc29196
Paulao17 <paul@shimaore.net>
2020-07-20 15:10:24 +0200

    Corrected url

--- buildFull.sh
+++ buildFull.sh
@@ -1,3 +1,3 @@
-sh buildIndex.sh
+sh buildIndex.sh "$1"
 
 #pandoc -o ex.html ex.md exos.md --toc -N -s --template=template.html --css=static/style.css
--- buildIndex.sh
+++ buildIndex.sh
@@ -1,3 +1,3 @@
 #pandoc -o  -t latex --toc -N -s ex.md exos.md
 
-pandoc -o build/index.html -s --template=templates/template-index.html src/index.md
+pandoc -o build/index.html -s --template=templates/template-index.html src/index.md $1
