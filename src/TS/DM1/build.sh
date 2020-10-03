57a3fafbb05b44479ed8da98e854a456c2fefb6a
Paul ALNET <paul@shimaore.net>
2020-10-03 15:47:01 +0200

    Remove table of contents from dm1

--- src/TS/DM1/build.sh
+++ src/TS/DM1/build.sh
@@ -4,5 +4,5 @@ echo "Building TS : DM 1"
 mkdir build/TS/DM1/ -p
 
 # Build pages & docs
-pandoc -o build/TS/DM1/dm1.html src/TS/DM1/dm1.md --toc -N -s --template=templates/template.html $1
-pandoc -o build/TS/DM1/dm1.pdf src/TS/DM1/dm1.md -t latex --toc -N -s  $1
+pandoc -o build/TS/DM1/dm1.html src/TS/DM1/dm1.md -N -s --template=templates/template.html $1
+pandoc -o build/TS/DM1/dm1.pdf src/TS/DM1/dm1.md -t latex -N -s  $1
